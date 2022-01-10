d13443078d3ce5cc34a28c2c100c2eca9822d397
AGD <aloeverdulay@gmail.com>
2022-01-10 15:42:35 +0800

    Added missing export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { useData } from './use-data/use_data'
 export { useLifecycle } from './use-lifecycle/use_lifecycle'
 export { usePrev } from './use-prev/use_prev'
 export { useStyle } from './use-style/use_style'
